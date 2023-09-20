d41a92d35731a4dcb1dcb324f9cb43a8f132ec63
kike <jerb5665@gmail.com>
2023-09-20 13:58:48 -0400

    fin de la seccion 4 del curso de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroModule } from './heroes/hero.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
